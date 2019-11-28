9c6b0cc5fc1321cefb651dcef8ad3e20e41058f3
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-28 11:09:30 +0430

    Youtap Library for NVP Message Formats

--- src/main/java/af/asr/youtap/message/BalanceGetRequest.java
+++ src/main/java/af/asr/youtap/message/BalanceGetRequest.java
@@ -1,4 +1,27 @@
 package af.asr.youtap.message;
 
 public class BalanceGetRequest {
+
+    private String messageType;
+    private String terminalId;
+    private String merchantId;
+    private String transactionId;
+    private String merchantPin;
+    private String customerData;
+    private String balanceType;
+
+
+    public static class Builder {
+
+
+        private String messageType;
+        private String terminalId;
+        private String merchantId;
+        private String transactionId;
+        private String merchantPin;
+        private String customerData;
+        private String balanceType;
+
+        
+    }
 }
