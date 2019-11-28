e4a3f339d8ec9152279db2cd925162a296cc45a3
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-28 14:47:22 +0430

    Youtap Library -- Messages

--- src/main/java/af/asr/youtap/util/CustomerData.java
+++ src/main/java/af/asr/youtap/util/CustomerData.java
@@ -91,4 +91,57 @@ public class CustomerData {
      * Customer Type
      */
     private CustomerType customerType;
+
+    /**
+     * Subscriber ID verified or not
+     *
+     * Fixed values are: ‘true’ or ‘false’ and are
+     * returned in the CustomerCreateResp
+     */
+    private  String idVarified;
+
+    /**
+     * New mobile money account PIN for a PIN
+     * change. In production systems this is
+     * encrypted within a pinblock
+     *
+     * New mobile money account PIN
+     */
+    private int newMobMonPin;
+
+    /**
+     * First additional finger print data
+     *
+     *First additional finger print data in
+     * BCT_ISO_FMR f ormat
+     *
+     * HEX Format
+     */
+    private String fingerData1;
+
+    /**
+     * Second additional finger print data in
+     * BCT_ISO_FMR f ormat
+     *
+     * Hex Format
+     */
+    private  String fingerData2;
+
+    /**
+     * Third additional finger print data in
+     * BCT_ISO_FMR f ormat
+     *
+     * Hex Format
+     */
+    private  String fingerData3;
+
+
+    /**
+     * Links or unlinks a bank account from
+     * mobile money account
+     * ‘L’ – link account
+     * ‘U’ – Unlink account
+     */
+    private LinkType linkType;
+
 }
