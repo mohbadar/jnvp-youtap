11108af6123ecb30654f54bec741d570f2a784d3
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-28 10:53:52 +0430

    NVP Library

---
+++ src/main/java/af/asr/youtap/util/YoutapMessageTypes.java
@@ -0,0 +1,9 @@
+package af.asr.youtap.util;
+
+public interface YoutapMessageTypes {
+
+    public static String MERCHANT_LOGIN_REQUEST = "MerchantLogin";
+    public static String MERCHANT_LOGIN_RESPONSE = "MerchantLoginResp";
+    public static String STAFF_LOGIN_REQUEST = "StaffLogin";
+    public static String STAFF_LOGIN_RESPONSE = "StaffLoginResp";
+}
