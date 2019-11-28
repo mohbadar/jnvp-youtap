8266b572573c35d864c78a9d3385e81579889026
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-28 11:48:06 +0430

    Youtap Library -- Get Balance Request

---
+++ src/test/java/af/asr/youtap/message/MerchantLoginRequestTests.java
@@ -0,0 +1,27 @@
+package af.asr.youtap.message;
+
+import af.asr.youtap.util.YoutapMessageTypes;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public class MerchantLoginRequestTests {
+
+    private String EXPECTED_MERCHANT_LOGIN_REQUEST = "MessageType=MerchantLogin,TerminalId=21908856,MerchantPin=1234,MerchantId=86637,TransactionId=21908856";
+
+    @Test
+    public void generateExpectedMessage_whenTheSameParamsArePassed()
+    {
+        MerchantLoginRequest request = MerchantLoginRequest.Builder.newInstance()
+                .withMerchantId("86637")
+                .withMessageType(YoutapMessageTypes.MERCHANT_LOGIN_REQUEST)
+                .withMerchantPin("1234")
+                .withTerminalId("21908856")
+                .withTransactionId("0000000955")
+                .build();
+        boolean result = request.getMessage().equals(EXPECTED_MERCHANT_LOGIN_REQUEST);
+        Assertions.assertThat(result);
+    }
+}
