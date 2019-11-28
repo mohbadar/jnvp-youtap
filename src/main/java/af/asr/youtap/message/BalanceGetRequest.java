c497b46a6b64d1d5d26bdfe1153cee401cb925d9
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-28 11:19:58 +0430

    Youtap Library -- Get Balance Request

--- src/main/java/af/asr/youtap/message/BalanceGetRequest.java
+++ src/main/java/af/asr/youtap/message/BalanceGetRequest.java
@@ -39,8 +39,52 @@ public class BalanceGetRequest {
         }
 
 
-        
+        public Builder withMessageType(String messageType){
+            this.messageType= messageType;
+            return this;
+        }
 
+        public Builder withTerminalId(String terminalId){
+            this.terminalId = terminalId;
+            return this;
+        }
 
+        public Builder withMerchantId(String merchantId){
+            this.merchantId = merchantId;
+            return this;
+        }
+
+        public Builder withTransactionId(String transactionId)
+        {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder withMerchantPin(String merchantPin)
+        {
+            this.merchantPin = merchantPin;
+            return this;
+        }
+
+        public Builder withCustomerData(String customerData)
+        {
+            this.customerData = customerData;
+            return this;
+        }
+
+        public Builder balanceType(String balanceType)
+        {
+            this.balanceType = balanceType;
+            return this;
+        }
+
+        public BalanceGetRequest build(){
+            return new BalanceGetRequest(this);
+        }
+    }
+
+    public String getMessage()
+    {
+        return String.format("MessageType=%s,TransactionId=%s,TerminalId=%s,MerchantId=%s,MerchantPin=%s,BalanceType=%s", this.messageType, this.transactionId, this.terminalId, this.merchantId, this.merchantPin, this.balanceType);
     }
 }
