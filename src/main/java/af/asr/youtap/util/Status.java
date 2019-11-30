0e4315091cd56877935e65f98398e08333fa8ff2
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-30 19:14:34 +0430

    Youtap Library - Transaction Data

--- src/main/java/af/asr/youtap/util/Status.java
+++ src/main/java/af/asr/youtap/util/Status.java
@@ -1,5 +1,24 @@
 package af.asr.youtap.util;
 
+/**
+ *Status code values 2 and 3 are displayed
+ * and code values 1 and 4 are printed.
+ * 0 – Successful
+ * 1 – System error
+ * 2 – Provisioning error
+ * 3 – Input error
+ * 4 – Declined
+ * If the status is not successful the Message
+ * field must be present in the response
+ * message explaining the error.
+ *
+ * Example:
+ *
+ *
+ * Status=2
+ * Message=POSTe
+ * rminal Not Found
+ */
 public interface Status {
 
     public static int SUCCESSFUL = 0;
