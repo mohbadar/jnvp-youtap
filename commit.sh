be15317dc726129166a953100cdbf7f19556a262
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-30 09:40:53 +0430

    Youtap Library - Customer Transaction REQUEST

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 
 
 git add .
-git commit -m "Youtap Library - Customer RESET PIN REQUEST"
+git commit -m "Youtap Library - Customer Transaction REQUEST"
---
+++ src/main/java/af/asr/youtap/util/data/CustomerIdentificationData.java
@@ -0,0 +1,6 @@
+package af.asr.youtap.util.data;
+
+public class CustomerIdentificationData {
+
+
+}
