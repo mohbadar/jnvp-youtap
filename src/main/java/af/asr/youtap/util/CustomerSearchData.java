0f240fcd504de36259563181b7fb09c1aa0ca151
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-30 17:41:16 +0430

    Youtap Library - Customer Search Data

--- src/main/java/af/asr/youtap/util/CustomerSearchData.java
+++ src/main/java/af/asr/youtap/util/CustomerSearchData.java
@@ -2,6 +2,58 @@ package af.asr.youtap.util;
 
 public class CustomerSearchData {
 
+
+    /**
+     * NFC Card serial number - Hex
+     */
+    private String NFCTagId;
+
+    /**
+     * Subscribers first name
+     */
     private String givenName;
+
+    /**
+     * Subscribers surname
+     */
+    private String surename;
+
+    /**
+     * Date of birth in YYYYMMDD format
+     */
+    public String dob;
+
+    /**
+     * Subscribers ID number created during
+     * registration
+     */
+    private String customerId;
+
+    /**
+     * Mobile phone number
+     */
+    private String msisdn;
+
+    /**
+     * Magnetic card track 2 account number. 16
+     * to 19 digits
+     */
+    private String cardId;
+
+    /**
+     * Magnetic card track 2 expiry date
+     */
+    private String cardExpiry;
+
+    /**
+     * Mobile money account PIN
+     */
+    private int mobMonPin;
+
+    /**
+     * Another identifier for the customer, used in the context of
+     * smart mobile phone application
+     */
+    private String imsi;
 
 }
