68972c6c0839c688f2c181802fb341eb2589752e
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-30 09:15:04 +0430

    Youtap Library - Customer Change PIN REQUEST

--- src/main/java/af/asr/youtap/message/request/CustomerChangePinRequest.java
+++ src/main/java/af/asr/youtap/message/request/CustomerChangePinRequest.java
@@ -27,6 +27,44 @@ public class CustomerChangePinRequest {
         private String customerData;
 
 
+        public static Builder newInstance(){
+            return  new Builder();
+        }
+
+
+        public Builder withMessageType(String messageType){
+            this.messageType= messageType;
+            return this;
+        }
+
+        public Builder withTerminalId(String terminalId){
+            this.terminalId = terminalId;
+            return this;
+        }
+
+        public Builder withMerchantId(String merchantId){
+            this.merchantId = merchantId;
+            return this;
+        }
+
+        public Builder withTransactionId(String transactionId)
+        {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder withCustomerData(String customerData)
+        {
+            this.customerData = customerData;
+            return this;
+        }
+
+        public CustomerChangePinRequest build()
+        {
+            return new CustomerChangePinRequest(this);
+        }
+
+
     }
 
 }
