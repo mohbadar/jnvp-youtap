d6a5bf4636cee66f0091d600225c4e2d3e3693ac
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-02 11:20:30 +0430

    Youtap Library - Identification Submit Request Message

--- src/main/java/af/asr/youtap/message/request/IdentificationSubmitRequest.java
+++ src/main/java/af/asr/youtap/message/request/IdentificationSubmitRequest.java
@@ -1,5 +1,7 @@
 package af.asr.youtap.message.request;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 public class IdentificationSubmitRequest {
 
 
@@ -36,6 +38,64 @@ public class IdentificationSubmitRequest {
         private String idData;
         private String fingerData;
 
-    }
 
+        public static Builder newInstance(){
+            return  new Builder();
+        }
+
+
+        public Builder withMessageType(String messageType){
+            this.messageType= messageType;
+            return this;
+        }
+
+        public Builder withTerminalId(String terminalId){
+            this.terminalId = terminalId;
+            return this;
+        }
+
+        public Builder withMerchantId(String merchantId){
+            this.merchantId = merchantId;
+            return this;
+        }
+
+        public Builder withTransactionId(String transactionId)
+        {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder withCustomerSearchData(String customerData)
+        {
+            this.customerData = customerData;
+            return this;
+        }
+
+        public Builder withFingerData(String fingerData)
+        {
+            this.fingerData = fingerData;
+            return this;
+        }
+
+        public Builder withPaymentType(String customerId)
+        {
+            this.customerId = customerId;
+            return  this;
+        }
+
+
+        public Builder withIdData(String idData)
+        {
+            this.idData = idData;
+            return  this;
+        }
+
+
+        public IdentificationSubmitRequest build()
+        {
+            return new IdentificationSubmitRequest(this);
+        }
+
+    }
+    
 }
