6c5bad91b30d2c6e3fbfbd18d46dcd43e79c9414
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-04 15:13:30 +0430

    Youtap Library - Exchange Rate Transaction Request Message

--- src/main/java/af/asr/youtap/message/request/ExchangeTransactionRequest.java
+++ src/main/java/af/asr/youtap/message/request/ExchangeTransactionRequest.java
@@ -65,6 +65,128 @@ public class ExchangeTransactionRequest {
         private String billPayeeId;
         private String billPayeeReference;
         private String contactMSISDN;
+
+
+        public static Builder newInstance(){
+            return  new Builder();
+        }
+
+
+        public Builder withMessageType(String messageType){
+            this.messageType= messageType;
+            return this;
+        }
+
+        public Builder withTerminalId(String terminalId){
+            this.terminalId = terminalId;
+            return this;
+        }
+
+        public Builder withMerchantId(String merchantId){
+            this.merchantId = merchantId;
+            return this;
+        }
+
+        public Builder withTransactionId(String transactionId)
+        {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+
+        public Builder withCustomerId(String customerId)
+        {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder withPaymentType(String paymentType)
+        {
+            this.paymentType = paymentType;
+            return this;
+        }
+
+        public Builder withSourceCurrency(String sourceCurrency)
+        {
+            this.sourceCurrency = sourceCurrency;
+            return this;
+        }
+
+        public Builder withDestinationCurrency(String destinationCurrency)
+        {
+            this.destinationCurrency = destinationCurrency;
+            return this;
+        }
+
+        public Builder withWorkingCurrency(String workingCurrency)
+        {
+            this.workingCurrency = workingCurrency;
+            return this;
+        }
+
+        public Builder withStaffPin(String staffPin)
+        {
+            this.staffPin = staffPin;
+            return this;
+        }
+
+        public Builder withSendingAmountExclFees(double sendingAmountExclFees)
+        {
+            this.sendingAmountExclFees = sendingAmountExclFees;
+            return this;
+        }
+
+        public Builder withFxRate(String fxRate)
+        {
+            this.fxRate = fxRate;
+            return  this;
+        }
+
+        public Builder withFee(String fee)
+        {
+            this.fee  = fee;
+            return this;
+        }
+
+        public Builder withCostToSend(String costToSend)
+        {
+            this.costToSend = costToSend;
+            return this;
+        }
+
+        public Builder withReceiveAmount(double receiveAmount)
+        {
+            this.receiveAmount = receiveAmount;
+            return this;
+        }
+
+        public Builder withBillPayeeId(String billPayeeId)
+        {
+            this.billPayeeId = billPayeeId;
+            return this;
+        }
+
+        public Builder withBillPayeeReference(String billPayeeReference)
+        {
+            this.billPayeeReference = billPayeeReference;
+            return this;
+        }
+
+        public Builder withContactMSISDN(String contactMSISDN)
+        {
+            this.contactMSISDN = contactMSISDN;
+            return this;
+        }
+
+        public ExchangeTransactionRequest build()
+        {
+            return new ExchangeTransactionRequest(this);
+        }
+    }
+
+    public String getMessage()
+    {
+        return  String.format("");
     }
 
 }
