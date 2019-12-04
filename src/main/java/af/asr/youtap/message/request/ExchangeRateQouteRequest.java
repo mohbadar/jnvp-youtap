3d21acf2a31a4edf2cd6bb116e6e61ffb94a39d7
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-04 14:42:34 +0430

    Youtap Library - Exchange Rate Qoute Request Message

--- src/main/java/af/asr/youtap/message/request/ExchangeRateQouteRequest.java
+++ src/main/java/af/asr/youtap/message/request/ExchangeRateQouteRequest.java
@@ -1,4 +1,47 @@
 package af.asr.youtap.message.request;
 
 public class ExchangeRateQouteRequest {
+
+    private String messageType;
+    private String terminalId;
+    private String merchantId;
+    private String transactionId;
+    private String customerId;
+    private String paymentType;
+    private String sourceCurrency;
+    private String destinationCurrency;
+    private String workingCurrency;
+    private String customerData;
+    private double workingAmount;
+
+    private ExchangeRateQouteRequest(Builder builder)
+    {
+        this.messageType = builder.messageType;
+        this.terminalId = builder.terminalId;
+        this.merchantId = builder.merchantId;
+        this.transactionId = builder.transactionId;
+        this.customerData = builder.customerData;
+        this.customerId = builder.customerId;
+        this.paymentType = builder.paymentType;
+        this.sourceCurrency = builder.sourceCurrency;
+        this.destinationCurrency  = builder.destinationCurrency;
+        this.workingCurrency = builder.workingCurrency;
+        this.workingAmount = builder.workingAmount;
+        
+    }
+
+    public static class Builder {
+
+        private String messageType;
+        private String terminalId;
+        private String merchantId;
+        private String transactionId;
+        private String customerId;
+        private String paymentType;
+        private String sourceCurrency;
+        private String destinationCurrency;
+        private String workingCurrency;
+        private String customerData;
+        private double workingAmount;
+    }
 }
