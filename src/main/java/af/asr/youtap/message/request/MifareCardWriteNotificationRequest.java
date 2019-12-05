8d6fd52d57ab15206aa345ec3d65d8780000f49a
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-05 10:54:47 +0430

    Youtap Library - EPurse Request  Messages

--- src/main/java/af/asr/youtap/message/request/MifareCardWriteNotificationRequest.java
+++ src/main/java/af/asr/youtap/message/request/MifareCardWriteNotificationRequest.java
@@ -1,4 +1,39 @@
 package af.asr.youtap.message.request;
 
+import lombok.Data;
+
+@Data
+/**
+ * Indicates the resulting prepaid card update status.
+ */
 public class MifareCardWriteNotificationRequest {
+
+    private String messageType;
+    private String terminalId;
+    private String merchantId;
+    private String transactionId;
+    private String cardType;
+    private String context;
+    private String customerData;
+    /**
+     * Status of the NFC Card write operation
+     */
+    private String commandStatus;
+
+    /**
+     * Transaction sequence reference number
+     */
+    private String writeReference;
+
+
+    /**
+     * MessageType=MifCardWriteNotification,TransactionId=0000000030,TerminalId=98378265,M
+     * erchantId=8888888888888,CardType=MIFARECLASSIC,Context=MEePurse,CustomerData=(NFCTa
+     * gId=521BFD5B),WriteReference=5976024341534212105,CommandStatus=NOTOK
+     * @return
+     */
+    public String getMessage()
+    {
+        return String.format("");
+    }
 }
