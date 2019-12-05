7b85c7ab100d089277ab49b31d1ca76734f8973f
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-05 11:27:26 +0430

    Youtap Library - Requesting approval from the server to perform a purse transaction with an EPurse card.

--- src/main/java/af/asr/youtap/message/request/EPurseTransactionAuthorizeRequest.java
+++ src/main/java/af/asr/youtap/message/request/EPurseTransactionAuthorizeRequest.java
@@ -20,6 +20,8 @@ public class EPurseTransactionAuthorizeRequest {
     private String paymentType;
     private String workingCurrency;
     private double workingAmount;
+    private String date;
+    private String time;
     /**
      * EPurse Details
      */
@@ -28,14 +30,19 @@ public class EPurseTransactionAuthorizeRequest {
      * The EPurseLog raw data to be written on the card
      */
     private String ePurseLog;
+    private String app;
 
+    /**
+     * MessageType=EPurseTransactionAuthorize,TransactionId=0000000030,TerminalId=98378265
+     * ,MerchantId=8888888888888,CardType=MIFARECLASSIC,Context=MEePurse,CustomerData=(NFC
+     * TagId=39BD361E),EPurseLog=965BBB40060084000000009487656701DAD313DC01CDBB9AB34955C16
+     * F8E98CA,PaymentType=TOPUP,WorkingAmount=100,Date=03/05/2016,Time=12:29:11,MERCHANTP
+     * IN=****,App=v1.8b73a,WorkingCurrency=IDR,EPurseData=(EPurseId=39BD361E,EPurseCurren
+     * cy=IDR,EPurseBalance=95807,EPurseCounter=7ffffff0)
+     * @return
+     */
     public String getMessage()
     {
-        return String.format("MessageType=EPurseTransactionAuthorize,TransactionId=0000000030,TerminalId=98378265\n" +
-                ",MerchantId=8888888888888,CardType=MIFARECLASSIC,Context=MEePurse,CustomerData=(NFC\n" +
-                "TagId=39BD361E),EPurseLog=965BBB40060084000000009487656701DAD313DC01CDBB9AB34955C16\n" +
-                "F8E98CA,PaymentType=TOPUP,WorkingAmount=100,Date=03/05/2016,Time=12:29:11,MERCHANTP\n" +
-                "IN=****,App=v1.8b73a,WorkingCurrency=IDR,EPurseData=(EPurseId=39BD361E,EPurseCurren\n" +
-                "cy=IDR,EPurseBalance=95807,EPurseCounter=7ffffff0)");
+        return String.format("MessageType=%s,TransactionId=%s,TerminalId=%s,MerchantId=%s,CardType=%s,Context=%s,CustomerData=%s,EPurseLog=%s,PaymentType=%s,WorkingAmount=%f,Date=%s,Time=%s,MERCHANTPIN=%s,App=%s,WorkingCurrency=%s,EPurseData=%s", this.messageType, this.transactionId, this.terminalId, this.merchantId, this.cardType, this.context, this.customerData, this.ePurseLog, this.paymentType, this.workingAmount, this.date,this.time, this.merchantPin, this.app, this.workingCurrency, this.ePurseData);
     }
 }
